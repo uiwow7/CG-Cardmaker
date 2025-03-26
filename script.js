656a0bdb6636b6fe9e1656b29466999e5a691d88
uiwow7 <aydinanginer087@gmail.com>
2025-03-25 17:36:15 -0700

    Change font

--- script.js
+++ script.js
@@ -159,7 +159,7 @@ function onFileSelected(event) {
 function arcTextInput() {
 	var arcplanet = document.getElementById("archetypeinputdropdown").value;
 	var text = document.getElementById("archetypeinput").value;
-	document.getElementById("text").innerHTML += `<span class="shaimus ${arcplanet.toLowerCase()}-rules-text" style="letter-spacing: 2px; transform: scaleX(1.2);">${text}</span><span>&nbsp;</span>`;
+	document.getElementById("text").innerHTML += `<span class="shaimus ${arcplanet.toLowerCase()}-rules-text">${text}</span><span>&nbsp;</span>`;
 }
 
 function textSizeInput() {
